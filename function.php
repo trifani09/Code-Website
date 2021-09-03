d1f937c88d717cc48fb400b2c2e9c3e6efd13761
Trifani Febrina Hasibuan <58590058+trifani09@users.noreply.github.com>
2021-09-03 17:50:02 +0700

    Delete function.php

--- function.php
+++
@@ -1,9 +0,0 @@
-<?php
-	global $conn;
-	$conn = mysqli_connect("localhost", "root", "", "dinas_pertanian");
-
-	if(!$conn){
-		die("Masalah Pada Database");
-	}
-	$db_use = mysqli_select_db($conn, "dinas_pertanian") or die("Select Database Problem !!");
-?>
